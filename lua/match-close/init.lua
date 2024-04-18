48e9b7db202a2e5fc44ec6b51bfe6a49bf1cf5df
Masgharoth <maksymilian.turski@gmail.com>
2024-04-18 16:01:33 +0200

    Initial commit

---
+++ lua/match-close/init.lua
@@ -0,0 +1,69 @@
+local function match_close(reference)
+	if type(reference) ~= "string" then
+		print("Bad input" .. reference .. type(reference))
+		return
+	end
+	local w = vim.api.nvim_get_current_line()
+	local window = vim.api.nvim_get_current_win()
+	local pos = vim.api.nvim_win_get_cursor(window)
+	local i = pos[2]
+	local j = 0
+	for c in w:gmatch(".") do
+		if j == i then
+			if c == reference then
+				--print 'should move'
+				vim.opt.virtualedit = "onemore"
+				vim.cmd("normal l")
+				return
+			else
+				--print 'should type'
+				if reference == '"' or reference == "'" then
+					vim.cmd.normal({ "i" .. reference .. reference, bang = true })
+					return
+				end
+
+				vim.cmd.normal({ "i" .. reference, bang = true })
+				vim.cmd("normal l")
+				return
+			end
+		else
+			if i == string.len(w) then
+				--print 'should type but on EOL'
+				if reference == '"' or reference == "'" then
+					vim.cmd.normal({ "i" .. reference .. reference, bang = true })
+					return
+				end
+				vim.cmd.normal({ "i" .. reference, bang = true })
+				vim.cmd("normal l")
+				return
+			end
+			--print 'got elsed'
+		end
+		j = j + 1
+	end
+end
+vim.keymap.set("i", ">", function()
+	match_close(">")
+end)
+
+vim.keymap.set("i", "}", function()
+	match_close("}")
+end)
+
+vim.keymap.set("i", ")", function()
+	match_close(")")
+end)
+
+vim.keymap.set("i", "]", function()
+	match_close("]")
+end)
+
+vim.keymap.set("i", "'", function()
+	match_close("'")
+end)
+
+vim.keymap.set("i", '"', function()
+	match_close('"')
+end)
+
+return {}
